f7cbd1af47222474c6c4511a71b0022de064ace0
Daniel Pope <mauve@mauveweb.co.uk>
2014-05-11 13:04:47 +0100

    New: initial scene with ship

--- bitsofeight/game.py
+++ bitsofeight/game.py
@@ -1,7 +1,15 @@
 import pyglet
 from pyglet.window import key
 from pyglet.event import EventDispatcher, EVENT_HANDLED
-from pyglet import gl
+
+
+from euclid import Point3
+
+from wasabisg.plane import Plane
+from wasabisg.scenegraph import Camera, Scene, v3, ModelNode
+from wasabisg.loaders.objloader import ObjFileLoader
+from wasabisg.model import Material
+from wasabisg.lighting import Sunlight
 
 
 WIDTH = 1024
@@ -10,61 +18,74 @@ HEIGHT = 600
 FPS = 60
 
 
+model_loader = ObjFileLoader()
+ship_model = model_loader.load_obj('assets/models/ship.obj')
+
+
+
 class World(EventDispatcher):
-    def __init__(self, keyboard):
-        self.keyboard = keyboard
+    def __init__(self):
         self.objects = []
 
+        self.create_scene()
+        self.camera = Camera(
+            pos=Point3(10, 10, 10),
+            look_at=Point3(0, 1, 0),
+            width=WIDTH,
+            height=HEIGHT
+        )
+
+    def update(self, dt):
+        """Update the world through the given time step (in seconds)."""
+
+    def create_scene(self):
+        """Initialise the scene with static objects."""
+        self.scene = Scene(
+            ambient=(0.1, 0.15, 0.2, 1.0),
+        )
+        # Sun
+        self.scene.add(Sunlight(
+            direction=(1, 1, 1),
+            colour=(1.0, 0.8, 0.5, 1.0),
+            intensity=1,
+        ))
+        # Sea
+        self.scene.add(
+            Plane(
+                size=1000,
+                material=Material(
+                    name='sea',
+                    Kd=(0.2, 0.4, 0.6)
+                )
+            )
+        )
+
+        # Ship, created statically for now
+        ship = ModelNode(ship_model, pos=(0, 0, 0))
+        self.scene.add(ship)
+
     def draw(self):
-        # draw a black background
-        gl.glClearColor(0, 0, 0, 1)
-        gl.glClear(gl.GL_COLOR_BUFFER_BIT | gl.GL_DEPTH_BUFFER_BIT)
-#        gl.glMatrixMode(gl.GL_PROJECTION)
-#        gl.glLoadIdentity()
-        gl.glMatrixMode(gl.GL_MODELVIEW)
-        gl.glLoadIdentity()
-        gl.glDisable(gl.GL_DEPTH_TEST)
-#        self.camera.set_matrix()
-        
-        gl.glEnable(gl.GL_TEXTURE_2D)
-        gl.glEnable(gl.GL_BLEND)
-
-#        vp = self.camera.get_viewport()
-
+        self.scene.render(self.camera)
 
 
 class GameState(object):
     def __init__(self, game):
         self.game = game
         self.window = game.window
-        self.keyboard = key.KeyStateHandler()
-
-        # load the sprites for objects
-        #load_all()
-
-        # initialise the World and start the game
-        self.world = World(self.keyboard)
-        #self.world.set_handler('on_player_death', self.on_player_death)
+        self.world = World()
 
     def start(self):
-        self.window.push_handlers(
-            self.keyboard
-        )
-        self.window.push_handlers(self.on_key_press)
         pyglet.clock.schedule_interval(self.update, 1.0 / FPS)
-        self.start_mission()
 
     def stop(self):
-        self.window.pop_handlers()
         self.window.pop_handlers()
         pyglet.clock.unschedule(self.update)
 
     def draw(self):
         self.world.draw()
 
-    def update(self, ts):
-        pass
-        #self.world.update(ts)
+    def update(self, dt):
+        self.world.update(dt)
 
 
 class Game(object):
@@ -79,44 +100,25 @@ class Game(object):
             self.window = pyglet.window.Window(fullscreen=True)
             WIDTH = self.window.width
             HEIGHT = self.window.height
-        self.game = None
-        self.menu = None
-        self.gamestate = None
-
-        self.window.push_handlers(self.on_draw)
-
-    def start_menu(self):
         self.gamestate = GameState(self)
-
-    def run(self):
-        while True:
-            try:
-                pyglet.app.run()
-            except Exception:
-                import traceback
-                traceback.print_exc()
-            else:
-                break
+        self.gamestate.start()
+        self.window.push_handlers(self.on_draw)
 
     def on_draw(self):
         self.gamestate.draw()
-
 
 
 def main():
     from optparse import OptionParser
     parser = OptionParser('%prog [-f]')
     parser.add_option('-f', '--fullscreen', action='store_true', help='Start in full screen mode')
-    
+
     options, args = parser.parse_args()
 
     game = Game(
         windowed=not options.fullscreen
     )
-    
-    game.start_menu()
-
-    game.run()
+    pyglet.app.run()
 
 
 if __name__ == '__main__':
